0ef5e11e622867302aba4bb958412198bd629ea8
Tom Davis <tom@toms-mbp.lan>
2023-11-06 17:04:03 -0500

    handle connect wallet click

--- app/src/Header.js
+++ app/src/Header.js
@@ -10,6 +10,10 @@ const Header = () => {
     let displayAccount = 'Connect Wallet';
     if(account) displayAccount = `${account.substring(0,5)}...${account.substring(account.length-3,account.length)}`;
 
+    const connect = () => {
+        navigate('/');
+    }
+
     return (
         <div className={classes.header_area}>
             <div className={classes.logo} onClick={()=>navigate('/')}>
@@ -18,7 +22,8 @@ const Header = () => {
             </div>
             <div className={classes.wallet_area}>
                 <span className={classes.eth_balance_area}>{`${Math.round(balanceETH*1000)/1000} ETH`}</span>
-                <span className={classes.wallet} onClick={()=>navigate(`/account/${account}`)}>{displayAccount}</span>
+                { account && <span className={classes.wallet} onClick={()=>navigate(`/account/${account}`)}>{displayAccount}</span>}
+                { !account && <span className={classes.wallet} onClick={()=>connect()}>{displayAccount}</span>}
             </div>
         </div>
     )
